bf292cc6ca585cef9c04bc7557ea056e1ce47fc5
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:01:21 +0530

    Initial Commit

---
+++ immunosuppression_trough_agent/__init__.py
@@ -0,0 +1,5 @@
+"""
+TransplantTDM Sentinel: Tacrolimus, Cyclosporine & Everolimus Trough Agent
+Maintains target whole-blood immunosuppressant trough windows (ng/mL) post-transplant, auditing CYP3A5 genotypes and drug interactions.
+"""
+__version__ = "2.0.0-PRO"
